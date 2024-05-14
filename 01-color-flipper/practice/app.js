3f7400f6f17e254b91b99033005c3c68ef042ab7
yuki <yukihiguchi0826@gmail.com>
2024-05-13 18:22:07 -0700

    simple js done

--- 01-color-flipper/practice/app.js
+++ 01-color-flipper/practice/app.js
@@ -1,1 +1,11 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const color = document.querySelector(".color")
+const button = document.querySelector("#btn")
+const main = document.querySelector("main")
+
+button.addEventListener("click", () => {
+    const thisColor = colors[Math.floor(Math.random() * 4)]
+    color.textContent = thisColor
+    main.style.backgroundColor = thisColor
+})
