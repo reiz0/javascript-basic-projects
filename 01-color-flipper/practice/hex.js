1d7d843802af520608a0263de2e4f3fc08316189
yuki <yukihiguchi0826@gmail.com>
2024-05-13 18:27:55 -0700

    hex done

--- 01-color-flipper/practice/hex.js
+++ 01-color-flipper/practice/hex.js
@@ -1,1 +1,20 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const createColor = () => {
+    let hexNums = "#"
+    for (let i = 0; i < 6; i++) {
+        const hexNum = hex[Math.floor(Math.random() * hex.length)]
+        hexNums += `${hexNum}`
+    }
+    return hexNums
+}
+
+const color = document.querySelector(".color")
+const button = document.querySelector("#btn")
+const main = document.querySelector("main")
+
+button.addEventListener("click", () => {
+    const thisColor = createColor()
+    color.textContent = thisColor
+    main.style.backgroundColor = thisColor
+})
