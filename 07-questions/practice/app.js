9a13125f83ffc9a916d4d9193cddba795a693fbd
yuki <yukihiguchi0826@gmail.com>
2024-06-05 19:55:46 -0700

    done adding toggle

--- 07-questions/practice/app.js
+++ 07-questions/practice/app.js
@@ -1,2 +1,13 @@
 //using selectors inside the element
 // traversing the dom
+$(() => {
+    $(".minus-icon").hide()
+    $(".question-text").hide()
+
+    $(".question-btn").on("click", function(){
+        console.log(this)
+        $(this).parent().parent().find(".question-text").toggle()
+        $(this).find(".plus-icon").toggle()
+        $(this).find(".minus-icon").toggle()
+    })
+})
